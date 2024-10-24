16535b396a501552b737a78b85b788de86ba2cab
Novasker <novasker@hotmail.com>
2024-10-24 13:03:15 -0300

    Bubble e Selection

--- SorterVisualizer/MainWindow.xaml.cs
+++ SorterVisualizer/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,7 +22,10 @@ public partial class MainWindow : Window
     private const int RECTANGLE_MAX_HEIGHT = 300;
     private List<Rectangle> rectangles = new List<Rectangle>();
     private Random random = new Random();
+    private int currentStep = 0;
+    private int currentIndex = 0;
     private bool isPaused = false;
+    private bool isRunning = false;
     
     
     public MainWindow()
@@ -30,6 +34,13 @@ public partial class MainWindow : Window
     }
     private void BtnStart_Click(object sender, RoutedEventArgs e)
     {
+        if (isRunning) return;
+        
+        currentIndex = 0;
+        currentStep = 0;
+        isPaused = false; 
+        isRunning = true;
+        
         string[] input = TextBox.Text.Split(',');
         int[] heights = new int[input.Length];
         bool validInput = true;
@@ -55,8 +66,25 @@ public partial class MainWindow : Window
                 heights[j] = random.Next(20, RECTANGLE_MAX_HEIGHT);
             }
         }
-
+        
         InitializeRectangles(heights);
+        
+        string selectedAlgorithm = (AlgorithmSelector.SelectedItem as ComboBoxItem).Content.ToString();
+
+        switch (selectedAlgorithm)
+        {
+            case "Bubble Sort":
+                InitializeBubbleSort();
+                break;
+            case "Selection Sort":
+                InitializeSelectionSort();
+                break;
+        }
+    }
+    
+    private void BtnPause_Click(object sender, RoutedEventArgs e)
+    {
+        isPaused = !isPaused;
     }
     
     private void InitializeRectangles(int[] heights)
@@ -70,7 +98,7 @@ public partial class MainWindow : Window
             {
                 Width = RECTANGLE_WIDTH,
                 Height = heights[i],
-                Fill = System.Windows.Media.Brushes.Blue
+                Fill = Brushes.Blue
             };
             
             Canvas.SetLeft(rect, i * (RECTANGLE_WIDTH + 10));
@@ -80,6 +108,110 @@ public partial class MainWindow : Window
             CanvasMain.Children.Add(rect); 
         }
     }
-
     
+    private async Task InitializeBubbleSort()
+    {
+        await Task.Delay(1000);
+
+        while (currentIndex < rectangles.Count - 1)
+        {
+            if (currentStep < rectangles.Count - currentIndex - 1)
+            {
+                if (!isPaused) 
+                {
+                    if (currentStep < rectangles.Count - 1 - currentIndex)
+                    {
+                        var rect1 = rectangles[currentStep];
+                        var rect2 = rectangles[currentStep + 1];
+
+                        if (GetHeight(rect1) > GetHeight(rect2))
+                        {
+                            await Swap(currentStep, currentStep + 1);
+                        }
+
+                        await Task.Delay(100); 
+                    }
+                    currentStep++;
+                }
+                else
+                {
+                    await Task.Delay(100);
+                }
+            }
+            else
+            {
+                currentStep = 0; 
+                currentIndex++; 
+            }
+        }
+
+        isRunning = false; 
+        MessageBox.Show("Ordenação Bubble Sort concluída!");
+    }
+    
+    private async Task InitializeSelectionSort()
+    {
+        await Task.Delay(1000);
+
+        while (currentIndex < rectangles.Count - 1)
+        {
+            if (!isPaused)
+            {
+                int minIndex = currentIndex;
+
+                for (int j = currentIndex + 1; j < rectangles.Count; j++)
+                {
+                    if (GetHeight(rectangles[j]) < GetHeight(rectangles[minIndex]))
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != currentIndex)
+                {
+                    await Swap(currentIndex, minIndex);
+                }
+
+                currentIndex++;
+            }
+            else
+            {
+                await Task.Delay(100); 
+            }
+        }
+
+        isRunning = false;
+        MessageBox.Show("Ordenação Selection Sort concluída!");
+    }
+    
+    private double GetHeight(Rectangle rect)
+    {
+        return rect.Height;
+    }
+    
+    private async Task Swap(int index1, int index2)
+    {
+        (rectangles[index1], rectangles[index2]) = (rectangles[index2], rectangles[index1]);
+
+        DoubleAnimation animation1 = new DoubleAnimation
+        {
+            From = Canvas.GetLeft(rectangles[index1]),
+            To = index1 * (RECTANGLE_WIDTH + 10),
+            Duration = TimeSpan.FromSeconds(0.5)
+        };
+
+        DoubleAnimation animation2 = new DoubleAnimation
+        {
+            From = Canvas.GetLeft(rectangles[index2]),
+            To = index2 * (RECTANGLE_WIDTH + 10),
+            Duration = TimeSpan.FromSeconds(0.5)
+        };
+        
+        rectangles[index1].BeginAnimation(Canvas.LeftProperty, animation1);
+        
+        rectangles[index2].BeginAnimation(Canvas.LeftProperty, animation2);
+        
+        await Task.Delay(500); 
+    }
 }
+
