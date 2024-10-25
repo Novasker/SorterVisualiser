83e7db9f91149c5e510c073a732690080fe23ff1
Novaski <novasker@hotmail.com>
2024-10-25 00:08:10 -0300

    Mais métodos de sort

--- SorterVisualizer/MainWindow.xaml.cs
+++ SorterVisualizer/MainWindow.xaml.cs
@@ -34,51 +34,76 @@ public partial class MainWindow : Window
     }
     private void BtnStart_Click(object sender, RoutedEventArgs e)
     {
-        if (isRunning) return;
-        
-        currentIndex = 0;
-        currentStep = 0;
-        isPaused = false; 
-        isRunning = true;
-        
-        string[] input = TextBox.Text.Split(',');
-        int[] heights = new int[input.Length];
-        bool validInput = true;
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (int.TryParse(input[i].Trim(), out int height)) 
-            {
-                heights[i] = height * RECTANGLE_HEIGHT_FACTOR;
-            }
-            else
-            {
-                validInput = false; 
-                break;
-            }
+        if (isRunning)
+        {   
+            isRunning = false;
+            rectangles.Clear();
+            CanvasMain.Children.Clear();
         }
-        
-        if (!validInput)
+        else
         {
-            heights = new int[10]; 
-            for (int j = 0; j < heights.Length; j++)
-            {
-                heights[j] = random.Next(20, RECTANGLE_MAX_HEIGHT);
-            }
-        }
-        
-        InitializeRectangles(heights);
-        
-        string selectedAlgorithm = (AlgorithmSelector.SelectedItem as ComboBoxItem).Content.ToString();
+            currentIndex = 0;
+            currentStep = 0;
+            isPaused = false;
+            isRunning = true;
 
-        switch (selectedAlgorithm)
-        {
-            case "Bubble Sort":
-                InitializeBubbleSort();
-                break;
-            case "Selection Sort":
-                InitializeSelectionSort();
-                break;
+            string[] input = TextBox.Text.Split(',');
+            int[] heights = new int[input.Length];
+            bool validInput = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (int.TryParse(input[i].Trim(), out int height))
+                {
+                    heights[i] = height * RECTANGLE_HEIGHT_FACTOR;
+                }
+                else
+                {
+                    validInput = false;
+                    break;
+                }
+            }
+
+            if (!validInput)
+            {
+                heights = new int[15];
+                for (int j = 0; j < heights.Length; j++)
+                {
+                    heights[j] = random.Next(20, RECTANGLE_MAX_HEIGHT);
+                }
+            }
+
+            InitializeRectangles(heights);
+
+            string selectedAlgorithm = (AlgorithmSelector.SelectedItem as ComboBoxItem).Content.ToString();
+
+            switch (selectedAlgorithm)
+            {
+                case "Bubble Sort":
+                    InitializeBubbleSort();
+                    break;
+                case "Selection Sort":
+                    InitializeSelectionSort();
+                    break;
+                case "Merge Sort":
+                    InitializeMergeSort();
+                    break;
+                case "Quick Sort":
+                    InitializeQuickSort();
+                    break;
+                case "Insertion Sort":
+                    InitializeInsertionSort();
+                    break;
+                case "Cocktail Shaker Sort":
+                    InitializeCocktailShakerSort();
+                    break;
+                case "Bogo Sort":
+                    InitializeBogoSort();
+                    break;
+                case "Miracle Sort":
+                    InitializeMiracleSort();
+                    break;
+            }
         }
     }
     
@@ -183,10 +208,120 @@ public partial class MainWindow : Window
         isRunning = false;
         MessageBox.Show("Ordenação Selection Sort concluída!");
     }
-    
-    private double GetHeight(Rectangle rect)
+    private async Task InitializeMergeSort()
     {
-        return rect.Height;
+        await Task.Delay(1000);
+        await MergeSort(0, rectangles.Count - 1);
+        isRunning = false;
+        MessageBox.Show("Ordenação Merge Sort concluída!");
+    }
+    private async Task InitializeQuickSort()
+    {
+        await Task.Delay(1000); 
+        await QuickSort(0, rectangles.Count - 1); 
+        isRunning = false;
+        MessageBox.Show("Ordenação Quick Sort concluída!");
+    }
+    private async Task InitializeInsertionSort()
+    {
+        await Task.Delay(1000); 
+        
+        for (int i = 1; i < rectangles.Count; i++)
+        {
+            var keyRect = rectangles[i];
+            double keyHeight = GetHeight(keyRect);
+
+            int j = i - 1;
+            
+            while (j >= 0 && GetHeight(rectangles[j]) > keyHeight)
+            {
+                await Swap(j, j + 1); 
+                j--;
+            }
+        }
+
+        isRunning = false;
+        MessageBox.Show("Ordenação Insertion Sort concluída!");
+    }
+    private async Task InitializeCocktailShakerSort()
+    {
+        await Task.Delay(1000); 
+
+        bool swapped = true;
+        int start = 0;
+        int end = rectangles.Count - 1;
+
+        while (swapped)
+        {
+            swapped = false;
+
+         
+            for (int i = start; i < end; i++)
+            {
+                if (GetHeight(rectangles[i]) > GetHeight(rectangles[i + 1]))
+                {
+                    await Swap(i, i + 1); 
+                    swapped = true; 
+                }
+            }
+
+ 
+            if (!swapped)
+                break;
+
+
+            end--;
+
+            swapped = false;
+
+
+            for (int i = end - 1; i >= start; i--)
+            {
+                if (GetHeight(rectangles[i]) > GetHeight(rectangles[i + 1]))
+                {
+                    await Swap(i, i + 1); 
+                    swapped = true; 
+                }
+            }
+            
+            start++;
+        }
+
+        isRunning = false;
+        MessageBox.Show("Ordenação Cocktail Shaker Sort concluída!");
+    }
+    private async Task InitializeBogoSort()
+    {
+        await Task.Delay(1000);
+
+        while (!IsSorted())
+        {
+            if (!isPaused)
+            {
+                ShuffleRectangles();
+                
+                await Task.Delay(1000); 
+            }
+            else
+            {
+                await Task.Delay(100); 
+            }
+        }
+
+        isRunning = false;
+        MessageBox.Show("Ordenação Bogo Sort concluída!");
+    }
+    private async Task InitializeMiracleSort()
+    {
+        await Task.Delay(1000);
+
+        while (!IsSorted())
+        {
+            await Task.Delay(100);
+        }
+
+        isRunning = false;
+        MessageBox.Show("Ordenação Miracle Sort concluída!");
     }
     
     private async Task Swap(int index1, int index2)
@@ -212,6 +347,118 @@ public partial class MainWindow : Window
         rectangles[index2].BeginAnimation(Canvas.LeftProperty, animation2);
         
         await Task.Delay(500); 
+    }
+    private double GetHeight(Rectangle rect)
+    {
+        return rect.Height;
+    }
+    private async Task MergeSort(int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = (left + right) / 2;
+            
+            await MergeSort(left, mid);
+            await MergeSort(mid + 1, right);
+            
+            await Merge(left, mid, right);
+        }
+    }
+
+    private async Task Merge(int left, int mid, int right)
+    {
+        int leftIndex = left;
+        int rightIndex = mid + 1;
+
+        List<Rectangle> temp = new List<Rectangle>();
+        
+        while (leftIndex <= mid && rightIndex <= right)
+        {
+            if (GetHeight(rectangles[leftIndex]) <= GetHeight(rectangles[rightIndex]))
+            {
+                temp.Add(rectangles[leftIndex]);
+                leftIndex++;
+            }
+            else
+            {
+                temp.Add(rectangles[rightIndex]);
+                rightIndex++;
+            }
+        }
+        
+        while (leftIndex <= mid)
+        {
+            temp.Add(rectangles[leftIndex]);
+            leftIndex++;
+        }
+        
+        while (rightIndex <= right)
+        {
+            temp.Add(rectangles[rightIndex]);
+            rightIndex++;
+        }
+        
+        for (int i = left; i <= right; i++)
+        {
+            int tempIndex = i - left;
+
+            if (rectangles[i] != temp[tempIndex])
+            {
+                await Swap(i, FindIndexOfRectangle(temp[tempIndex]));
+            }
+        }
+    }
+    private int FindIndexOfRectangle(Rectangle rect)
+    {
+        return rectangles.IndexOf(rect);
+    }
+    private bool IsSorted()
+    {
+        for (int i = 0; i < rectangles.Count - 1; i++)
+        {
+            if (GetHeight(rectangles[i]) > GetHeight(rectangles[i + 1]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    private async void ShuffleRectangles()
+    {
+        for (int i = 0; i < rectangles.Count; i++)
+        {
+            int randomIndex = random.Next(0, rectangles.Count);
+            await Swap(i, randomIndex);
+        }
+    }
+    private async Task QuickSort(int low, int high)
+    {
+        if (low < high)
+        {
+            int pivotIndex = await Partition(low, high);
+            
+            await QuickSort(low, pivotIndex - 1);
+            await QuickSort(pivotIndex + 1, high);
+        }
+    }
+    private async Task<int> Partition(int low, int high)
+    {
+        var pivot = rectangles[high];
+        double pivotHeight = GetHeight(pivot);
+
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (GetHeight(rectangles[j]) < pivotHeight)
+            {
+                i++;
+                await Swap(i, j); 
+            }
+        }
+        
+        await Swap(i + 1, high);
+        return i + 1; 
     }
 }
 
